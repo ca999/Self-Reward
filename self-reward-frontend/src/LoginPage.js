e28a3212e9d0df446faa58145a660db58bed7ffe
Nayana <nayanasajiscr@gmail.com>
2025-01-11 11:32:32 +0530

    backend & frontend connection

--- self-reward-frontend/src/LoginPage.js
+++ self-reward-frontend/src/LoginPage.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import {jwtDecode} from "jwt-decode";
+import axios from "axios";
 
 
 function LoginPage() {
   const [user, setUser] = useState(null);
 
-  const handleLoginSuccess = (response) => {
+   const handleLoginSuccess = async (response) => {
     console.log("Google login successful:", response);
     const userObject = jwtDecode(response.credential); // Decoding the JWT token
     setUser(userObject);
+
+    // Send the user details to the backend
+    try {
+      const backendResponse = await axios.post('http://127.0.0.1:8000/home/', {
+        email: userObject.email,
+        name: userObject.name,
+        picture: userObject.picture,
+      });
+
+      console.log("Backend response:", backendResponse.data);
+    } catch (error) {
+      console.error("Error calling the backend:", error);
+    }
   };
 
   const handleLoginFailure = (error) => {
@@ -18,7 +32,7 @@ function LoginPage() {
 
   return (
     <div className="login-container">
-      <h1>Welcome to ProDUCKtivity</h1>
+      <h1>Welcome to Steadigain</h1>
       <br/>
       <h2>Login</h2>
       <br/>
