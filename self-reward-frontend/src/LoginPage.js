0674382913270924dac52268328fcf0871548409
Nayana <nayanasajiscr@gmail.com>
2025-01-12 13:50:03 +0530

    login persistence + logout feature

--- self-reward-frontend/src/LoginPage.js
+++ self-reward-frontend/src/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import {jwtDecode} from "jwt-decode";
 
@@ -6,9 +6,25 @@ import {jwtDecode} from "jwt-decode";
 function LoginPage() {
   const [user, setUser] = useState(null);
 
+   useEffect(() => {
+    // Check if user data exists in localStorage
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser)); // Parse and set the user state
+    }
+  }, []);
+
+   const handleLogout = () => {
+    // Clear user data from localStorage
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
   const handleLoginSuccess = (response) => {
     console.log("Google login successful:", response);
-    const userObject = jwtDecode(response.credential); // Decoding the JWT token
+    const userObject = jwtDecode(response.credential);
+    localStorage.setItem('user', JSON.stringify(userObject));
+
     setUser(userObject);
   };
 
@@ -17,23 +33,28 @@ function LoginPage() {
   };
 
   return (
-    <div className="login-container">
-      <h1>Welcome to ProDUCKtivity</h1>
-      <br/>
-      <h2>Login</h2>
-      <br/>
-      {!user ? (
-        <GoogleLogin
-          onSuccess={handleLoginSuccess}
-          onError={handleLoginFailure}
-        />
-      ) : (
-        <div>
-          <h3>Welcome, {user.name}!</h3>
-          <img src={user.picture} alt={user.name} />
-        </div>
-      )}
-    </div>
+      <div className="login-container">
+          <h1>Welcome to ProDUCKtivity</h1>
+          <br/>
+          <h2>Login</h2>
+          <br/>
+          {!user ? (
+              <GoogleLogin
+                  onSuccess={handleLoginSuccess}
+                  onError={handleLoginFailure}
+              />
+          ) : (
+              <div>
+                  <h3>Welcome, {user.name}!</h3>
+                  <img src={user.picture} alt={user.name}/>
+              </div>
+          )}
+
+          <br/>
+
+          {user && <button onClick={handleLogout}>Logout</button>}
+
+      </div>
   );
 }
 
